747b05f6827eb3000bcb9aafe77e7f492d0f44ec
Rawld Gill <rgill@altoviso.com>
2018-09-01 17:34:33 -0700

    rfe: enhance to allow setting then signaling of multiple watch variables, e.g., (set, set, signal, signal) rather than (set, signal, set, signal)

--- src/WatchHub.js
+++ src/WatchHub.js
@@ -1,8 +1,23 @@
 const ppVariables = Symbol("WatchHub-ppVariables");
 
+function mutate(owner, name, privateName, newValue){
+	let oldValue = owner[name];
+	if(!newValue || !oldValue ? newValue !== oldValue : (newValue.eq ? !newValue.eq(oldValue) : (oldValue.eq ? !oldValue.eq(newValue) : newValue !== oldValue))){
+		if(privateName in owner){
+			owner[privateName] = newValue;
+		}else{
+			Object.defineProperty(owner, privateName, {writable: true, value: newValue});
+		}
+		return [name, oldValue, newValue];
+	}else{
+		return false;
+	}
+}
+
 export default function WatchHub(superClass){
 	if(!superClass){
-		superClass = class {};
+		superClass = class {
+		};
 	}
 	return class extends superClass {
 		constructor(){
@@ -19,13 +34,16 @@ export default function WatchHub(superClass){
 		}
 
 		_applyWatchers(name, privateName, newValue){
-			let oldValue = this[privateName];
-			if(oldValue !== newValue){
-				this[privateName] = newValue;
-				this._applyWatchersRaw(name, oldValue, newValue);
-				return true;
+			if(arguments.length > 3){
+				let i = 0;
+				let results = [];
+				while(i < arguments.length){
+					results.push(mutate(this, arguments[i++], arguments[i++], arguments[i++]));
+				}
+				for(const p of results) p && this._applyWatchersRaw(...p);
 			}else{
-				return false;
+				let result = mutate(this, name, privateName, newValue);
+				result && this._applyWatchersRaw(...result);
 			}
 		}
 
