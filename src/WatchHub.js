81802725dde86a914ab9fe4ec832b7dc48f19ea5
Rawld Gill <rgill@altoviso.com>
2018-09-01 17:35:50 -0700

    rfe: create watcher catalog only if at least one watch is actually requested

--- src/WatchHub.js
+++ src/WatchHub.js
@@ -22,13 +22,12 @@ export default function WatchHub(superClass){
 	return class extends superClass {
 		constructor(){
 			super();
-			Object.defineProperty(this, ppVariables, {value: {}});
 		}
 
 		// protected interface...
 		_applyWatchersRaw(name, oldValue, newValue){
-			let watchers = this[ppVariables][name];
-			if(watchers){
+			let watchers;
+			if(this[ppVariables] && (watchers = this[ppVariables][name])){
 				watchers.slice().forEach(w => w.watcher(newValue, oldValue, this));
 			}
 		}
@@ -49,35 +48,37 @@ export default function WatchHub(superClass){
 
 		// public interface...
 		watch(name, watcher){
+			if(!this[ppVariables]){
+				Object.defineProperty(this, ppVariables, {value: {}, configurable:true});
+			}
 			if(!watcher){
 				let hash = name;
 				return Reflect.ownKeys(hash).map((name) => this.watch(name, hash[name]));
 			}else if(Array.isArray(name)){
-				return name.map((name)=>this.watch(name, watcher));
+				return name.map((name) => this.watch(name, watcher));
 			}else{
 				let watchers = this[ppVariables][name] || (this[ppVariables][name] = []);
 				let wrappedwatcher = {watcher: watcher};
 				watchers.push(wrappedwatcher);
 				return {
-					destroy: () =>{
+					destroy: () => {
 						let watchers = this[ppVariables][name];
 						let index = watchers ? watchers.indexOf(wrappedwatcher) : -1;
 						if(index !== -1){
 							watchers.splice(index, 1);
 						}
 					}
-				}
+				};
 			}
 		}
 
 		destroyWatch(name){
-			if(name){
-				delete this[ppVariables][name];
-			}else{
-				let watches = this[ppVariables];
-				Reflect.ownKeys(watches).forEach((key) =>{
-					delete watches[key];
-				});
+			if(this[ppVariables]){
+				if(name){
+					delete this[ppVariables][name];
+				}else{
+					delete this[ppVariables];
+				}
 			}
 		}
 	};
