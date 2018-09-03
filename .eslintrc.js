40e1158bbeb2f784153c7d6432daaee2b96563e3
Rawld Gill <rgill@altoviso.com>
2018-09-03 14:29:10 -0700

    rfe: add missing eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab",
+			{ "SwitchCase": 1 }
+        ],
+        "no-undef":["warn", { "typeof": false }],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
