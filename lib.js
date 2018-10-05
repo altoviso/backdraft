9eb68c271b8e2043d1da2b655bc6214add09a3f8
Rawld Gill <rgill@altoviso.com>
2018-10-04 19:40:30 -0700

    rfe: update lib.js to reflect refactor

--- lib.js
+++ lib.js
@@ -1,14 +1,16 @@
-import {create, insert} from  "./src/dom.js"
-import Component from "./src/Component.js"
+import {create, insert} from  "./src/dom.js";
+import Component from "./src/Component.js";
 Component.createNode = create;
 Component.insertNode = insert;
 
-export * from "./src/dom.js"
-export {default as e, Element} from "./src/element.js"
-export {default as EventHub} from "./src/EventHub.js"
-export {default as WatchHub} from "./src/WatchHub.js"
-export {render} from "./src/Component.js"
-export {Component}
+export * from "./src/dom.js";
+export {default as e, svg, Element} from "./src/element.js";
+export {default as EventHub} from "./src/EventHub.js";
+export * from "./src/destroyable.js";
+export * from "./src/watchUtils.js";
+export {render} from "./src/Component.js";
+export {Component};
+export {default as Collection} from "./src/Collection.js";
 
 export function version(){
 	return "2.2.1";
