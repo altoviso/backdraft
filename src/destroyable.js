0e295e06088935d03f1ad9d030ab01457e1cdb5a
Rawld Gill <rgill@altoviso.com>
2018-10-24 14:34:13 -0700

    rfe: make container optional for destroyables

--- src/destroyable.js
+++ src/destroyable.js
@@ -2,23 +2,28 @@ function noop(){
 }
 
 function destroyable(proc, container, onEmpty){
-	let result = {
-		proc: proc,
-		destroy(){
+	let result = {proc: proc};
+	if(container){
+		result.destroy = () => {
 			result.destroy = result.proc = noop;
 			let index = container.indexOf(result);
 			if(index !== -1){
 				container.splice(index, 1);
 			}
 			!container.length && onEmpty && onEmpty();
-		}
-	};
-	container.push(result);
+		};
+		container.push(result);
+	}else{
+		result.destroy = () => {
+			result.destroy = result.proc = noop;
+		};
+	}
+
 	return result;
 }
 
 function destroyAll(container){
-	for(let i= 0, end = container.length; i<end && container.length; i++){
+	for(let i = 0, end = container.length; i < end && container.length; i++){
 		container.pop().destroy();
 	}
 }
