6f6c2bf4674d9d423ebf603bf30a9d409b0cb0df
rcgill <rgill@altoviso.com>
2019-05-23 11:12:45 -0700

    rfe: improve destroyAll to consume non-existing containers

--- src/destroyable.js
+++ src/destroyable.js
@@ -1,31 +1,35 @@
-function noop(){
+function noop() {
 }
 
-function destroyable(proc, container, onEmpty){
-	let result = {proc: proc};
-	if(container){
-		result.destroy = () => {
-			result.destroy = result.proc = noop;
-			let index = container.indexOf(result);
-			if(index !== -1){
-				container.splice(index, 1);
-			}
-			!container.length && onEmpty && onEmpty();
-		};
-		container.push(result);
-	}else{
-		result.destroy = () => {
-			result.destroy = result.proc = noop;
-		};
-	}
+function destroyable(proc, container, onEmpty) {
+    let result = {proc: proc};
+    if (container) {
+        result.destroy = () => {
+            result.destroy = result.proc = noop;
+            let index = container.indexOf(result);
+            if (index !== -1) {
+                container.splice(index, 1);
+            }
+            !container.length && onEmpty && onEmpty();
+        };
+        container.push(result);
+    } else {
+        result.destroy = () => {
+            result.destroy = result.proc = noop;
+        };
+    }
 
-	return result;
+    return result;
 }
 
-function destroyAll(container){
-	for(let i = 0, end = container.length; i < end && container.length; i++){
-		container.pop().destroy();
-	}
+function destroyAll(container) {
+    // deterministic algorithm to destroy handles that works even when handle destructors
+    // cause another handles to be destroyed
+    if (Array.isArray(container)) {
+        for (let i = 0, end = container.length; i < end && container.length; i++) {
+            container.pop().destroy();
+        }
+    }// else container was likely falsey and never used
 }
 
 export {destroyable, destroyAll};
