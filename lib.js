81c2d895a963d668ff83925b63d5722c803da92e
rcgill <rgill@altoviso.com>
2019-05-28 08:45:47 -0700

    rfe: update rollup file to reflect recent refactoring

--- lib.js
+++ lib.js
@@ -1,18 +1,23 @@
-import {create, insert} from  "./src/dom.js";
-import {initialize} from "./src/Component.js";
-initialize(document, create, insert);
+import {setGlobal} from "./src/global.js";
+
+setGlobal(window);
 
 import {element} from "./src/element.js";
+
 export {element as e};
 
+export * from "./src/global.js";
 export * from "./src/symbols.js";
+export * from "./src/destroyable.js";
 export * from "./src/postProcessingCatalog.js";
 export * from "./src/element.js";
-export * from "./src/destroyable.js";
 export * from "./src/eventHub.js";
 export * from "./src/watchUtils.js";
+export * from "./src/dom.js";
+export * from "./src/focusManager.js";
+export * from "./src/viewportWatcher.js";
 export * from "./src/Component.js";
 export * from "./src/Collection.js";
-export * from "./src/dom.js";
+export * from "./src/postProcessingFunctions.js";
 
-export const version = "2.3.1";
+export const version = "3.0.0";
