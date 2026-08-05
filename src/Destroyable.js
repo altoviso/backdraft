56313bab2b9b55a085354970fecbc3b06138afe7
Rawld Gill <rgill@altoviso.com>
2026-08-05 15:42:20 -0700

    feat(Destroyable): Add pause and onEmpty features
    * Add feature to pause callbacks.
    
    * Add feature set provide a callback to be applied when the container is emptied.
    
    * Add feature to apply a configurable function upon detecting an exception when
      applying the callback associated with a destroyable.

--- src/Destroyable.js
+++ src/Destroyable.js
@@ -1,36 +1,113 @@
-function noop() {
-    // do nothing
+let unexpectedHandler = function (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+};
+
+export function setUnexpectedHandler(h) {
+    const oldHandler = unexpectedHandler;
+    unexpectedHandler = h;
+    return oldHandler;
 }
 
-class Destroyable {
+export class Destroyable {
     constructor(proc, container, onEmpty) {
-        const result = this;
-        result.proc = proc;
+        this._proc = proc;
         if (container) {
-            result.destroy = () => {
-                result.destroy = result.proc = noop;
-                const index = container.indexOf(result);
-                if (index !== -1) {
-                    container.splice(index, 1);
-                }
-                !container.length && onEmpty && onEmpty();
-            };
-            container.push(result);
-        } else {
-            result.destroy = () => {
-                result.destroy = result.proc = noop;
-            };
+            (this.container = container).push(this);
+        }
+        if (onEmpty) {
+            this.onEmpty = onEmpty;
         }
     }
 
-    static destroyAll(container) {
-        // deterministic and robust algorithm to destroy handles:
-        //   * deterministic even when handle destructors insert handles (though the new handles will not be destroyed)
-        //   * robust even when handle destructors cause other handles to be destroyed
-        if (Array.isArray(container)) {
-            container.slice().forEach(h => h.destroy());
-        }// else container was likely falsy and never used
+    proc(...args) {
+        try {
+            !this._pause && !this._pauseOnce && this._proc && this._proc(...args);
+            this._pauseOnce && (this._pauseOnce = 0);
+        } catch (e) {
+            unexpectedHandler(e);
+        }
+        return this;
+    }
+
+    exec() {
+        this.proc();
+        return this;
+    }
+
+    pause(proc) {
+        if (proc) {
+            this.pause();
+            try {
+                proc();
+            } catch (e) {
+                unexpectedHandler(e);
+            }
+            this.unpause();
+            return this;
+        }
+        if (!this._pause) {
+            this._pause = 1;
+        } else {
+            ++this._pause;
+        }
+        return this;
+    }
+
+    pauseOnce() {
+        this._pauseOnce = true;
+        return this;
+    }
+
+    unpause() {
+        this._pause && --this._pause;
+        return this;
+    }
+
+    unpauseAll() {
+        this._pause = this._pauseOnce = 0;
+        return this;
+    }
+
+    destroy() {
+        if (this.onDestroy) {
+            try {
+                this.onDestroy();
+                delete this.onDestroy;
+            } catch (e) {
+                // squelch
+                unexpectedHandler(e);
+            }
+        }
+        delete this._proc;
+        const container = this.container;
+        if (container) {
+            const index = container.indexOf(this);
+            if (index !== -1) {
+                container.splice(index, 1);
+            }
+            if (!container.length) {
+                if (this.onEmpty) {
+                    try {
+                        this.onEmpty();
+                    } catch (e) {
+                        // squelch
+                        unexpectedHandler(e);
+                    }
+                }
+                if (container.onEmpty) {
+                    try {
+                        container.onEmpty();
+                    } catch (e) {
+                        // squelch
+                        unexpectedHandler(e);
+                    }
+                }
+            }
+        }
     }
 }
 
-export {Destroyable};
+export function destroyable(proc, container, onEmpty) {
+    return new Destroyable(proc, container, onEmpty);
+}
