9ed213353cd4101173bd1d275269e05ff159b4e5
Rawld Gill <rgill@altoviso.com>
2026-08-05 15:42:21 -0700

    feat(bdPromise): Add promise post-processing function

--- src/postProcessingFunctions.js
+++ src/postProcessingFunctions.js
@@ -1,7 +1,7 @@
-import {insPostProcessingFunction} from './postProcessingCatalog.js';
-import {getWatchableRef} from './watchUtils.js';
-import {connect, setAttr} from './dom.js';
-import {Component} from './Component.js';
+import { Component } from './Component.js';
+import { connect, setAttr } from './dom.js';
+import { insPostProcessingFunction } from './postProcessingCatalog.js';
+import { getWatchableRef } from './watchUtils.js';
 
 insPostProcessingFunction(
     'bdAttach',
@@ -31,12 +31,12 @@ insPostProcessingFunction(
         return value;
     },
     (ppfOwner, ppfTarget, watchers) => {
-        Reflect.ownKeys(watchers).forEach(eventType => {
-            let watcher = watchers[eventType];
+        Reflect.ownKeys(watchers).forEach(name => {
+            let watcher = watchers[name];
             if (typeof watcher !== 'function') {
-                watcher = ppfOwner[eventType].bind(ppfOwner);
+                watcher = ppfOwner[name].bind(ppfOwner);
             }
-            ppfTarget.ownWhileRendered(ppfTarget.watch(eventType, watcher));
+            ppfTarget.ownWhileRendered(ppfTarget.watch(name, watcher));
         });
     }
 );
@@ -125,7 +125,7 @@ insPostProcessingFunction(
             }));
         }
 
-        args = args.slice();
+        args = Array.isArray(args) ? args.slice() : [args];
         let owner,
             prop;
         while (args.length) {
@@ -142,21 +142,28 @@ insPostProcessingFunction(
 );
 
 insPostProcessingFunction(
+    // for dom nodes, there is the synthetic attribute "inner" which says set the inner contents of the node as follows
+    // if the value is an object that has its 'asHtml' property set true, then set innerHTML; otherwise, set innerText.
+    //
+    // if the caller intends to *always* set one or the other innerHTML or innerText, then those explicit properties should be used
+    // it is only when the caller doesn't know...as is often the case when building generalized widgets
+
     'bdReflect',
     (prop, value) => {
         if (prop === null && value instanceof Object && !Array.isArray(value)) {
-            // e.g., bdReflect:{p1:"someProp", p2:[refObject, "someOtherProp", someFormatter]}
+            // e.g., bdReflect:{p1:"someProp" -or- [[refObject,] "someProp" [, someFormatter], ...], p2:...}
             return value;
         } else if (prop) {
-            // e.g., bdReflect_someProp: [refObject, ] prop [, someFormatter]
-            return {[prop]: value};
+            // e.g., bdReflect_someProp: "someProp" -or- [[refObject,] "someOtherProp" [, someFormatter], ...]
+            return { [prop]: value };
         } else {
-            // e.g., bdReflect: [refObject, ] prop [, someFormatter]
-            return {innerHTML: value};
+            // e.g., bdReflect: "someProp" -or- [[refObject,] "someOtherProp" [, someFormatter], ...]
+            return { innerHTML: value };
         }
     },
     (ppfOwner, ppfTarget, props) => {
-        // props is a hash from property in ppfTarget to a list of ([refObject, ] property, [, formatter])...
+        // props is a hash from property in ppfTarget to a property in ppfOwner or
+        // an array of [[refObject, ] property [, formatter], ...]
         let install,
             watchable;
         if (ppfTarget instanceof Component) {
@@ -170,10 +177,18 @@ insPostProcessingFunction(
         } else {
             install = (destProp, refObject, prop, formatter) => {
                 ppfOwner.ownWhileRendered((watchable = getWatchableRef(refObject, prop, formatter)));
-                setAttr(ppfTarget, destProp, watchable.value);
-                ppfOwner.ownWhileRendered(watchable.watch(newValue => {
-                    setAttr(ppfTarget, destProp, newValue);
-                }));
+                if (destProp === 'inner') {
+                    const value = watchable.value;
+                    setAttr(ppfTarget, value && value.asHtml ? 'innerHTML' : 'innerText', value);
+                    ppfOwner.ownWhileRendered(watchable.watch(newValue => {
+                        setAttr(ppfTarget, newValue && newValue.asHtml ? 'innerHTML' : 'innerText', newValue);
+                    }));
+                } else {
+                    setAttr(ppfTarget, destProp, watchable.value);
+                    ppfOwner.ownWhileRendered(watchable.watch(newValue => {
+                        setAttr(ppfTarget, destProp, newValue);
+                    }));
+                }
             };
         }
 
@@ -190,6 +205,71 @@ insPostProcessingFunction(
                     prop = args.shift();
                 }
                 install(destProp, refObject, prop, typeof args[0] === 'function' ? args.shift() : null);
+            }
+        });
+    }
+);
+
+const NO_INITIAL_VALUE = {};
+
+insPostProcessingFunction(
+    'bdPromise',
+    (prop, value) => {
+        if (prop === null && value instanceof Object && !(value instanceof Promise) && !Array.isArray(value)) {
+            // e.g., bdPromise:{p1:somePromise -or- [somePromise [, initialValue] [, someFormatter], ...]}
+            return value;
+        } else if (prop) {
+            // e.g., bdPromise_someProp: somePromise -or- [somePromise [, initialValue] [, someFormatter], ...]
+            return { [prop]: value };
+        } else {
+            // e.g., bdPromise: somePromise -or- [somePromise [, initialValue] [, someFormatter], ...]
+            return { innerHTML: value };
+        }
+    },
+    (ppfOwner, ppfTarget, props) => {
+        // props is a hash from property in ppfTarget to a promise or
+        // an array of [promise [, initialValue, ] [, formatter], ...]
+        let install;
+        if (ppfTarget instanceof Component) {
+            install = (destProp, promise, initialValue, formatter) => {
+                if (initialValue !== NO_INITIAL_VALUE) {
+                    ppfTarget[destProp] = initialValue;
+                }
+                promise.then(value => {
+                    ppfTarget[destProp] = formatter ? formatter(value) : value;
+                });
+            };
+        } else {
+            install = (destProp, promise, initialValue, formatter) => {
+                if (initialValue !== NO_INITIAL_VALUE) {
+                    setAttr(ppfTarget, destProp, initialValue);
+                }
+                promise.then(value => {
+                    setAttr(ppfTarget, destProp, formatter ? formatter(value) : value);
+                });
+            };
+        }
+
+        Reflect.ownKeys(props).forEach(destProp => {
+            const args = Array.isArray(props[destProp]) ? props[destProp].slice() : [props[destProp]];
+
+            while (args.length) {
+                let initialValue = NO_INITIAL_VALUE;
+                let formatter = 0;
+                const promise = args.shift();
+                if (args.length && !(args[0] instanceof Promise)) {
+                    // this promise comes with initialValue and/or formatter...
+                    initialValue = args.shift();
+                    if (typeof initialValue === 'function') {
+                        // just a formatter, no instanceof
+                        formatter = initialValue;
+                        initialValue = NO_INITIAL_VALUE;
+                    } else if (args.length && typeof args[0] === 'function') {
+                        // this promise comes with both initialValue and/or formatter...
+                        formatter = args.shift();
+                    }
+                }
+                install(destProp, promise, initialValue, formatter);
             }
         });
     }
